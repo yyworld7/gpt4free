7a53ef91bbc61d197d951457dd668cea31aa4cae
yyworld7 <103169143+yyworld7@users.noreply.github.com>
2023-08-19 20:34:44 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*- 
+
+import os
+import sys
+import json
+import random
+import string
+import time
+from typing import Any
+
+from flask import Flask, request
+from flask_cors import CORS
+
+# 获取当前脚本的绝对路径
+current_path = os.path.dirname(os.path.abspath(__file__))
+
+# 获取项目根目录的绝对路径
+project_root = os.path.abspath(os.path.join(current_path, '..'))
+
+# 将项目根目录添加到 Python 路径中
+sys.path.append(project_root)
+
+from g4f import ChatCompletion
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/v1/chat/completions", methods=["POST"])
+def chat_completions():
+    
+    model = request.get_json().get("model", "gpt-3.5-turbo")
+    stream = request.get_json().get("stream", False)
+    stream = False
+    messages = request.get_json().get("messages")
+
+    # 生成一个随机的暂停时间，范围为10到50秒
+    pause_time = random.randint(20, 80)
+
+    # 使用 time.sleep() 函数来暂停程序执行
+    time.sleep(pause_time)
+
+    response = ChatCompletion.create(model=model, stream=stream, messages=messages)
+
+    completion_id = "".join(random.choices(string.ascii_letters + string.digits, k=28))
+    completion_timestamp = int(time.time())
+
+    if not stream:
+        #response.charset = 'utf-8'
+        return {
+            "id": f"chatcmpl-{completion_id}",
+            "object": "chat.completion",
+            "created": completion_timestamp,
+            "model": model,
+            "choices": [
+                {
+                    "index": 0,
+                    "message": {
+                        "role": "assistant",
+                        "content": response,
+                    },
+                    "finish_reason": "stop",
+                }
+            ],
+            "usage": {
+                "prompt_tokens": None,
+                "completion_tokens": None,
+                "total_tokens": None,
+            },
+        }
+
+    def streaming():
+        for chunk in response:
+            completion_data = {
+                "id": f"chatcmpl-{completion_id}",
+                "object": "chat.completion.chunk",
+                "created": completion_timestamp,
+                "model": model,
+                "choices": [
+                    {
+                        "index": 0,
+                        "delta": {
+                            "content": chunk,
+                        },
+                        "finish_reason": None,
+                    }
+                ],
+            }
+
+            content = json.dumps(completion_data, separators=(",", ":"))
+            yield f"data: {content}\n\n"
+            time.sleep(0.1)
+
+        end_completion_data: dict[str, Any] = {
+            "id": f"chatcmpl-{completion_id}",
+            "object": "chat.completion.chunk",
+            "created": completion_timestamp,
+            "model": model,
+            "choices": [
+                {
+                    "index": 0,
+                    "delta": {},
+                    "finish_reason": "stop",
+                }
+            ],
+        }
+        content = json.dumps(end_completion_data, separators=(",", ":"))
+        yield f"data: {content}\n\n"
+
+    return app.response_class(streaming(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=9385, debug=True)
